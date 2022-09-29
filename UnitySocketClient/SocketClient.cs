f7e224092cdc34bf563c47fd1782ffd0b2db899c
SeockJu Shin <dkstlzu@gmail.com>
2022-09-29 14:35:52 +0900

    SocketClient Update
    UDP Bindable

--- UnitySocketClient/SocketClient.cs
+++ UnitySocketClient/SocketClient.cs
@@ -1,24 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.Remoting.Messaging;
 
 using UnityEngine;
 
 namespace UnitySocketClient
 {
-    public enum PrintMode
+    public class EmptyPacketException : System.Exception
     {
-        Console,
-        UnityEditor,
+        public override string Message => "Got Empty Packet terminate auto receive";
     }
 
     [System.Serializable]
-    public class SocketClient
+    public class SocketClient : IDisposable
     {
         public Socket Socket;
 
@@ -26,109 +23,170 @@ namespace UnitySocketClient
         public SocketType SocketType = SocketType.Stream;
         public ProtocolType ProtocolType = ProtocolType.Tcp;
         public string IP;
-        public int Port;
-        private IPEndPoint iPEndPoint;
+        public int TCPPort;
+        public int UDPPort;
+        public bool AutoRecieve;
+        IPEndPoint _IPEndPoint;
+        public bool LogMessages;
 
-        private Byte[] bytesReceived = new Byte[256];
+        private Byte[] bytesReceived = new Byte[2048];
         private int receivedBytesLength = 0;
-        public Queue<string> ResponsesQueue = new Queue<string>();
-        private string Response = "not yet";
+        private int _bytesBufferLength;
+        public int BytesBufferLength
+        {
+            get
+            {
+                return _bytesBufferLength;
+            }
+            set
+            {
+                _bytesBufferLength = Mathf.Min(bytesReceived.Length, value);
+            }
+        }
 
         public SocketError SendErrorCode;
         public SocketError ReceiveErrorCode;
 
         private Action<object> print = MonoBehaviour.print;
-        private Action<string> printLog = Debug.Log;
-        private Action<string> printWarning = Debug.LogWarning;
         private Action<string> printError = Debug.LogError;
 
-        public event Action<IAsyncResult> OnConnectComplete;
+        public event Action OnConnectComplete;
+        public event Action OnDisconnected;
+        public event Action<Byte[]> OnBytesRecieved;
+        public event Action<string> OnDataRecievedAsString;
+
+        List<IAsyncResult> _IAsyncResultList = new List<IAsyncResult>();
 
         public SocketClient() { }
-        public SocketClient(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType, IPEndPoint iPEndPoint) 
+        public SocketClient(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType, IPEndPoint ipEndPoint) 
         {
-            AddressFamily = addressFamily;
-            SocketType = socketType;
-            ProtocolType = protocolType;
-            SetSocket();
-
-            this.iPEndPoint = iPEndPoint;
-            SetEndPoint();
+            Init(addressFamily, SocketType, protocolType, ipEndPoint);
         }
 
         public SocketClient(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType, string iP, int port) 
         {
+            IP = iP;
+            TCPPort = port;
+            IPEndPoint _ipEndPoint = new IPEndPoint(IPAddress.Parse(iP), port);
+            Init(addressFamily, SocketType, protocolType, _ipEndPoint);
+        }
+
+        void Init(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType, IPEndPoint ipEndPoint)
+        {
             AddressFamily = addressFamily;
             SocketType = socketType;
             ProtocolType = protocolType;
             SetSocket();
 
-            IP = iP;
-            Port = port;
-            SetEndPoint();
-        }
-
-        public void SetPrintMode(PrintMode mode)
-        {
-            //print = mode switch
-            //{
-                //PrintMode.Console => Console.WirteLine
-            //}
+            _IPEndPoint = ipEndPoint;
         }
 
         public void SetSocket()
         {
             Socket = new Socket(AddressFamily, SocketType, ProtocolType);
+            print($"AddressFamily : {AddressFamily}, SocketType : {SocketType}, ProtocolType {ProtocolType}");
         }
 
-        public void SetEndPoint()
+        public void SetTCPEndPoint()
         {
-            iPEndPoint = new IPEndPoint(IPAddress.Parse(IP), Port);
-            MonoBehaviour.print(iPEndPoint);
+            _IPEndPoint = new IPEndPoint(IPAddress.Parse(IP), TCPPort);
+            print("SetTCPEndPoint : " + _IPEndPoint);
+        }
+
+        public void SetUDPEndPoint()
+        {
+            UDPPort = GetOpenPort();
+
+            if (UDPPort == 0)
+            {
+                UDPPort = GetOpenPort(8500, 500);
+            }
+
+            if (UDPPort == 0)
+            {
+                Debug.LogError("Can not assign open port to udp");
+            } else
+            {
+                print($"UDPPort assigned {UDPPort}");
+            }
+        }
+
+        public static int GetOpenPort(int start = 8000, int checkRange = 500)
+        {
+            int startingAtPort = start;
+            int maxNumberOfPortsToCheck = checkRange;
+            IEnumerable<int> range = Enumerable.Range(startingAtPort, maxNumberOfPortsToCheck);
+            IEnumerable<int> portsInUse = 
+                from p in range
+                    join used in System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().GetActiveUdpListeners()
+                        on p equals used.Port
+                            select p;
+
+            int FirstFreeUDPPortInRange = range.Except(portsInUse).FirstOrDefault();
+
+            return FirstFreeUDPPortInRange;
+        }
+
+        public void Bind(int port = -1)
+        {
+            if (Socket == null) SetSocket();
+
+            if (_IPEndPoint == null) SetTCPEndPoint();
+
+            try
+            {
+                if (port >= 0)
+                {
+                    Socket.Bind(new IPEndPoint(IPAddress.Any, port));
+                } else
+                {
+                  Socket.Bind(new IPEndPoint(IPAddress.Any, 0));
+                }
+
+                UDPPort = ((IPEndPoint)Socket.LocalEndPoint).Port;
+
+                if (AutoRecieve) UDPReceiveAsync();
+            } catch (Exception e)
+            {
+                Debug.LogError(e);
+            }
         }
 
         public bool Connect()
         {
             if (Socket == null) SetSocket();
 
-            if (iPEndPoint == null) SetEndPoint();
+            if (_IPEndPoint == null) SetTCPEndPoint();
 
-            Socket.Connect(iPEndPoint);
+            Socket.Connect(_IPEndPoint);
 
             
             if (!Socket.Connected)
             {
-                Debug.LogFormat("Connection failed");
+                print("Connection failed");
                 return false;
             }
             else
             {
-                Debug.LogFormat("Connection Success");
+                print("Connection Success");
+                OnConnectComplete?.Invoke();
                 return true;
             }
         }
 
-        public void ConnectAsync()
+        public IAsyncResult ConnectAsync()
         {
             if (Socket == null) SetSocket();
 
-            if (iPEndPoint == null) SetEndPoint();
+            if (_IPEndPoint == null) SetTCPEndPoint();
 
             print("Connecting to server");
 
-            Socket.BeginConnect(iPEndPoint, AfterConnectAsync, null);
-        }
+            var ar = Socket.BeginConnect(_IPEndPoint, AfterConnectAsync, null);
 
-        public void UnConnect()
-        {
-            Socket.Shutdown(SocketShutdown.Both);
-            Socket.Close();
-        }
+            _IAsyncResultList.Add(ar);
 
-        ~SocketClient()
-        {
-            Socket.Shutdown(SocketShutdown.Both);
-            Socket.Close();
+            return ar;
         }
 
         void AfterConnectAsync(IAsyncResult result)
@@ -136,18 +194,28 @@ namespace UnitySocketClient
             try
             {
                 Socket.EndConnect(result);
-                print($"Connect finish and result : {result}");
-                print($"Connect finish and result : {result.GetType()}");
+                print("Connection Success");
+                _IAsyncResultList.Remove(result);
+                OnConnectComplete?.Invoke();
+                if (AutoRecieve)
+                    TCPReceiveAsync();
             }
             catch (Exception e)
             {
-                Debug.LogError(e.ToString());
+                printError("Connection Failed" + e.ToString());
             }
+        }
+
+        public void UnConnect()
+        {
+            OnDisconnected?.Invoke();
+            Socket.Shutdown(SocketShutdown.Both);
+            Socket.Close();
         }
 
         public void UDPSend(byte[] bytes)
         {
-            Socket.SendTo(bytes, iPEndPoint);
+            Socket.SendTo(bytes, _IPEndPoint);
         }
 
         public void UDPSend(string str)
@@ -155,14 +223,18 @@ namespace UnitySocketClient
             UDPSend(UTF8FromString(str));
         }
 
-        public void UDPSendAsync(byte[] bytes)
+        public IAsyncResult UDPSendAsync(byte[] bytes)
         {
-            Socket.BeginSendTo(bytes, 0, bytes.Length, SocketFlags.None, iPEndPoint, AfterUDPSend, null);
+            var ar = Socket.BeginSendTo(bytes, 0, bytes.Length, SocketFlags.None, _IPEndPoint, AfterUDPSend, null);
+
+            _IAsyncResultList.Add(ar);
+
+            return ar;
         }
 
-        public void UDPSendAsync(string str)
+        public IAsyncResult UDPSendAsync(string str)
         {
-            UDPSendAsync(UTF8FromString(str));
+            return UDPSendAsync(UTF8FromString(str));
         }
 
         void AfterUDPSend(IAsyncResult result)
@@ -170,40 +242,62 @@ namespace UnitySocketClient
             try
             {
                 Socket.EndSendTo(result);
+                _IAsyncResultList.Remove(result);
             }
             catch (Exception e)
             {
-                Debug.LogError(e.ToString());
+                printError(e.ToString());
             }
         }
 
-        public void UDPReceive()
+        public string UDPReceive()
         {
-            EndPoint EP = (EndPoint)iPEndPoint;
-            Socket.ReceiveFrom(bytesReceived, 0, bytesReceived.Length, SocketFlags.None, ref EP);
+            EndPoint EP = (EndPoint)new IPEndPoint(IPAddress.Any, UDPPort);
+            string Response = string.Empty;
+            receivedBytesLength = Socket.ReceiveFrom(bytesReceived, 0, BytesBufferLength, SocketFlags.None, ref EP);
+            if (receivedBytesLength > 0)
+            {
+                Response = StringFromUTF8(bytesReceived);
+                if (LogMessages) print("UDPReceive : " + Response);
+            }
+            Array.Clear(bytesReceived, 0, BytesBufferLength);
+            return Response;
         }
         
-        public void UDPReceiveAsync()
+        public IAsyncResult UDPReceiveAsync()
         {
-            EndPoint EP = (EndPoint)iPEndPoint;
-            Socket.BeginReceiveFrom(bytesReceived, 0, bytesReceived.Length, SocketFlags.None, ref EP, AfterUDPRecieve, EP);
+            EndPoint EP = (EndPoint)new IPEndPoint(IPAddress.Any, UDPPort);
+            var ar = Socket.BeginReceiveFrom(bytesReceived, 0, BytesBufferLength, SocketFlags.None, ref EP, AfterUDPRecieve, EP);
+
+            _IAsyncResultList.Add(ar);
+            
+            return ar;
         }
 
         void AfterUDPRecieve(IAsyncResult result)
         {
             try
             {
-                EndPoint EP= (EndPoint)result.AsyncState;
+                EndPoint EP = (EndPoint)result.AsyncState;
                 int dataLength = Socket.EndReceiveFrom(result, ref EP);
                 if (dataLength > 0)
                 {
-                    ResponsesQueue.Enqueue(StringFromUTF8(bytesReceived));
-                    Array.Clear(bytesReceived, 0, bytesReceived.Length);
+                    string reply = StringFromUTF8(bytesReceived);
+                    if (reply == string.Empty) throw new EmptyPacketException();
+                    if (LogMessages) print("UDPReceiveAsync : " + reply);
+                    _IAsyncResultList.Remove(result);
+                    OnBytesRecieved?.Invoke(bytesReceived);
+                    OnDataRecievedAsString?.Invoke(reply);
+                    Array.Clear(bytesReceived, 0, BytesBufferLength);
+                    if (AutoRecieve) UDPReceiveAsync();
                 }
-            }
-            catch (Exception e)
+            } catch(EmptyPacketException e)
             {
-                Debug.LogError(e.ToString());
+                printError($"At {ProtocolType} : " + e.ToString());
+                AutoRecieve = false;
+            } catch (Exception e)
+            {
+                printError(e.ToString());
             }
         }
 
@@ -224,14 +318,18 @@ namespace UnitySocketClient
             return TCPSend(UTF8FromString(str));
         }
 
-        public void TCPSendAsync(byte[] bytes)
+        public IAsyncResult TCPSendAsync(byte[] bytes)
         {
-            Socket.BeginSend(bytes, 0, bytes.Length, SocketFlags.None, out SendErrorCode, AfterTCPSend, null);
+            var ar = Socket.BeginSend(bytes, 0, bytes.Length, SocketFlags.None, out SendErrorCode, AfterTCPSend, null);
+
+            _IAsyncResultList.Add(ar);
+            
+            return ar;
         }
 
-        public void TCPSendAsync(string str = null)
+        public IAsyncResult TCPSendAsync(string str = null)
         {
-            TCPSendAsync(UTF8FromString(str));
+            return TCPSendAsync(UTF8FromString(str));
         }
 
         void AfterTCPSend(IAsyncResult result)
@@ -239,28 +337,41 @@ namespace UnitySocketClient
             try
             {
                 Socket.EndSend(result, out SendErrorCode);
+                _IAsyncResultList.Remove(result);
             }
             catch (Exception e)
             {
-                Debug.LogError(e.ToString());
-                Debug.LogError(SendErrorCode.ToString());
+                printError(SendErrorCode.ToString() + " : " + e.ToString());
             }
         }
 
-        public SocketError TCPReceive()
+        public string TCPReceive()
         {
-            receivedBytesLength = Socket.Receive(bytesReceived, 0, bytesReceived.Length, SocketFlags.None, out ReceiveErrorCode);
+            receivedBytesLength = Socket.Receive(bytesReceived, 0, BytesBufferLength, SocketFlags.None, out ReceiveErrorCode);
+            string Response = string.Empty;
             if (ReceiveErrorCode != SocketError.Success)
             {
                 Debug.LogFormat(ReceiveErrorCode.ToString());
+            } else if (receivedBytesLength > 0)
+            {
+                Response = StringFromUTF8(bytesReceived);
+                if (LogMessages) print("TCPRecieve : " + Response);
             }
+            Array.Clear(bytesReceived, 0 , BytesBufferLength);
 
-            return ReceiveErrorCode;
+            return Response;
         }
 
-        public void TCPReceiveAsync()
+        public IAsyncResult TCPReceiveAsync(int length = 0)
         {
-            Socket.BeginReceive(bytesReceived, 0, bytesReceived.Length, SocketFlags.None, out ReceiveErrorCode, AfterTCPReceive, null);
+            if (length > 0)
+                BytesBufferLength = length;
+
+            var ar = Socket.BeginReceive(bytesReceived, 0, BytesBufferLength, SocketFlags.None, out ReceiveErrorCode, AfterTCPReceive, null);
+
+            _IAsyncResultList.Add(ar);
+            
+            return ar;
         }
 
         void AfterTCPReceive(IAsyncResult result)
@@ -268,13 +379,21 @@ namespace UnitySocketClient
             try
             {
                 Socket.EndReceive(result, out ReceiveErrorCode);
-                ResponsesQueue.Enqueue(StringFromUTF8(bytesReceived));
-                Array.Clear(bytesReceived, 0, bytesReceived.Length);
-            }
-            catch (Exception e)
+                string reply = StringFromUTF8(bytesReceived);
+                if (reply == string.Empty) throw new EmptyPacketException();
+                if (LogMessages) print("TCPReceiveAsync : " + _IPEndPoint + "\n" + reply);
+                _IAsyncResultList.Remove(result);
+                OnBytesRecieved?.Invoke(bytesReceived);
+                OnDataRecievedAsString?.Invoke(reply);                
+                Array.Clear(bytesReceived, 0, BytesBufferLength);
+                if (AutoRecieve) TCPReceiveAsync();
+            } catch (EmptyPacketException e)
             {
-                Debug.LogError(e.ToString());
-                Debug.LogError(SendErrorCode.ToString());
+                printError($"At {ProtocolType} : " + e.ToString());
+                AutoRecieve = false;
+            } catch (Exception e)
+            {
+                printError(ReceiveErrorCode.ToString() + $" of {ProtocolType} : " + e.ToString());
             }
         }
 
@@ -285,15 +404,13 @@ namespace UnitySocketClient
 
         string StringFromUTF8(byte[] bytes)
         {
-            return Encoding.UTF8.GetString(bytes, 0, bytes.Length);
-        }
-
-        public string GetResponse()
-        {
-            if (ResponsesQueue.Count == 0)
-                return string.Empty;
-            else
-                return ResponsesQueue.Dequeue();
+            string fullString = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+            int indexOfNullChar = fullString.IndexOf('\0');
+            if (indexOfNullChar >= 0)
+            {
+                fullString = fullString.Substring(0, indexOfNullChar);
+            }
+            return fullString;
         }
 
         void PrintByteArray(byte[] byteArr)
@@ -304,5 +421,18 @@ namespace UnitySocketClient
             }
         }
 
+        public void Dispose()
+        {
+            OnDisconnected?.Invoke();
+
+            AutoRecieve = false;
+            foreach(IAsyncResult ar in _IAsyncResultList)
+            {
+                ar.AsyncWaitHandle.Dispose();
+            }
+            _IAsyncResultList.Clear();
+            UnConnect();
+            Socket.Dispose();
+        }
     }
 }
